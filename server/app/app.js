1230d8a3642e7f76a2214db710bf5aa71ced38a8
Aimee Knight <AimeeKnight83@gmail.com>
2014-02-14 12:08:05 -0600

    start clean

--- server/app/app.js
+++ server/app/app.js
@@ -1,32 +1,24 @@
 'use strict';
 
-var dbname = 'imdb';
+var dbname = 'database-name';
 var port = process.env.PORT || 4000;
 
 var d = require('./lib/request-debug');
+var connectMongo = require('./lib/mongodb-connection-pool').initialize(dbname);
+
 var express = require('express');
 var home = require('./routes/home');
-var movies = require('./routes/movies');
 var app = express();
 
 /* --- pipeline begins */
-/* --- vertical --- */
-/* ensure connected to db */
-app.use(require('./lib/mongodb-connection-pool').initialize(dbname, app));
-/* ensure logging */
+app.use(connectMongo);
 app.use(express.logger(':remote-addr -> :method :url [:status]'));
 app.use(require('./lib/cors'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 
-/* --- horizontal --- */
 app.get('/', d, home.index);
-app.get('/movies', d, movies.index);
-app.post('/movies', d, movies.create);
-app.get('/movies/query', d, movies.query);
-app.delete('/movies/:id', d, movies.destroy);
-app.put('/movies/:id', d, movies.update);
 /* --- pipeline ends   */
 
 var server = require('http').createServer(app);
